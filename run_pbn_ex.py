457847aa684541f82a28fb5155b2f34f72180ff0
Ella Marrero <marreroella44@gmail.com>
2025-04-16 15:31:09 -0700

    Updated call of function to match new input names

--- run_pbn_ex.py
+++ run_pbn_ex.py
@@ -1,9 +1,9 @@
 from pbn import *
 
-img_path = f'{os.getcwd()}/data/sunset-2.jpeg'
+img_path = f'{os.getcwd()}/data/kayak.jpeg'
 outpath = f'{os.getcwd()}/output/'
-name = "Sunset at Tam"
+name = "Kayaking"
 crayon_box = 96
 no_crayon = True
 
-create_pbn(img_path, name, outpath, crayon_box = crayon_box, no_crayon = True)
+create_pbn(img_path, name, outpath, crayon_box = crayon_box, pre_crayon_save = True)
