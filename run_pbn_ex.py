22a14352046aa518168a3cb9914841f17eaa4503
Ella Marrero <marreroella44@gmail.com>
2025-04-22 14:37:23 -0700

    described inputs

--- run_pbn_ex.py
+++ run_pbn_ex.py
@@ -1,9 +1,9 @@
 from pbn import *
 
-img_path = f'{os.getcwd()}/data/kayak.jpeg'
-outpath = f'{os.getcwd()}/output/'
-name = "Kayaking"
-crayon_box = 96
-no_crayon = True
+img_path = f'{os.getcwd()}/data/kayak.jpeg' # image filepath, should be added to 'data' folder
+outpath = f'{os.getcwd()}/output/' # output pathfile
+name = "Kayaking" # name of image (will also be included in output files)
+crayon_box = 96 # number of crayons in box -- (16, 24, 48, 64, 96, 120)
+pre_crayon_save = True # whether or not you want a version of simplified image before colors are replaced with crayon colros
 
-create_pbn(img_path, name, outpath, crayon_box = crayon_box, pre_crayon_save = True)
+create_pbn(img_path, name, outpath, crayon_box = crayon_box, pre_crayon_save = pre_crayon_save)
